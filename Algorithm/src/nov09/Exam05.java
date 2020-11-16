55343bada010e447328566b623f09cc2c0898239
88jina <jina940323@gmail.com>
2020-11-16 10:02:57 +0900

    nov 16th algo study

--- Algorithm/src/nov09/Exam05.java
+++ Algorithm/src/nov09/Exam05.java
@@ -1,18 +1,21 @@
 package nov09;
 
-import java.util.Arrays;
-
 public class Exam05 {
 
-    public String solution(String[] participant, String[] completion) {
-        String answer = "";
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr1[0].length];
 
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-
-        
+        for(int i=0; i<arr1.length; i++){
+            for(int j =0; j<arr1[0].length; j++){
+                answer[i][j]=arr1[i][j]+arr2[i][j];
+            }
+        }
 
         return answer;
+    }
+
+    public static void main(String[] args) {
+
     }
 
 }
